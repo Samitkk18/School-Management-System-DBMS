c7d0ad22eaa319ec67eb69e70c848505ad3589c4
Samit Kapadia <samitkapadia@Samits-MacBook-Pro.local>
2020-11-08 21:56:35 +0530

    calendar file head change

--- calendar.php
+++ calendar.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
  <head>
-  <title>Jquery Fullcalandar Integration with PHP and Mysql</title>
+  <title>SMS-DBMS</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
   <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" /> -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
